ca33f2e1551c6d18b01c024c3c0221699cc91c27
Jason Reilly <jdr0887@renci.org>
2014-05-02 14:33:46 -0400

    adding dao classes

--- opm-dao/opm-dao-api/src/main/java/org/renci/opm/dao/OPMDAOBean.java
+++ opm-dao/opm-dao-api/src/main/java/org/renci/opm/dao/OPMDAOBean.java
@@ -2,8 +2,168 @@ package org.renci.opm.dao;
 
 public class OPMDAOBean {
 
+    private ActivityDAO activityDAO;
+
+    private DocumentDAO documentDAO;
+
+    private EndDAO endDAO;
+
+    private InternationalizedStringDAO internationalizedStringDAO;
+
+    private LocationDAO locationDAO;
+
+    private OrganizationDAO organizationDAO;
+
+    private OtherDAO otherDAO;
+
+    private PersonDAO personDAO;
+
+    private PrimarySourceDAO primarySourceDAO;
+
+    private QualifiedNameDAO qualifiedNameDAO;
+
+    private QuotationDAO quotationDAO;
+
+    private RevisionDAO revisionDAO;
+
+    private RoleDAO roleDAO;
+
+    private SoftwareAgentDAO softwareAgentDAO;
+
+    private StartDAO startDAO;
+
+    private TypeDAO typeDAO;
+
     public OPMDAOBean() {
         super();
+    }
+
+    public ActivityDAO getActivityDAO() {
+        return activityDAO;
+    }
+
+    public void setActivityDAO(ActivityDAO activityDAO) {
+        this.activityDAO = activityDAO;
+    }
+
+    public DocumentDAO getDocumentDAO() {
+        return documentDAO;
+    }
+
+    public void setDocumentDAO(DocumentDAO documentDAO) {
+        this.documentDAO = documentDAO;
+    }
+
+    public EndDAO getEndDAO() {
+        return endDAO;
+    }
+
+    public void setEndDAO(EndDAO endDAO) {
+        this.endDAO = endDAO;
+    }
+
+    public InternationalizedStringDAO getInternationalizedStringDAO() {
+        return internationalizedStringDAO;
+    }
+
+    public void setInternationalizedStringDAO(InternationalizedStringDAO internationalizedStringDAO) {
+        this.internationalizedStringDAO = internationalizedStringDAO;
+    }
+
+    public LocationDAO getLocationDAO() {
+        return locationDAO;
+    }
+
+    public void setLocationDAO(LocationDAO locationDAO) {
+        this.locationDAO = locationDAO;
+    }
+
+    public OrganizationDAO getOrganizationDAO() {
+        return organizationDAO;
+    }
+
+    public void setOrganizationDAO(OrganizationDAO organizationDAO) {
+        this.organizationDAO = organizationDAO;
+    }
+
+    public OtherDAO getOtherDAO() {
+        return otherDAO;
+    }
+
+    public void setOtherDAO(OtherDAO otherDAO) {
+        this.otherDAO = otherDAO;
+    }
+
+    public PersonDAO getPersonDAO() {
+        return personDAO;
+    }
+
+    public void setPersonDAO(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    public PrimarySourceDAO getPrimarySourceDAO() {
+        return primarySourceDAO;
+    }
+
+    public void setPrimarySourceDAO(PrimarySourceDAO primarySourceDAO) {
+        this.primarySourceDAO = primarySourceDAO;
+    }
+
+    public QualifiedNameDAO getQualifiedNameDAO() {
+        return qualifiedNameDAO;
+    }
+
+    public void setQualifiedNameDAO(QualifiedNameDAO qualifiedNameDAO) {
+        this.qualifiedNameDAO = qualifiedNameDAO;
+    }
+
+    public QuotationDAO getQuotationDAO() {
+        return quotationDAO;
+    }
+
+    public void setQuotationDAO(QuotationDAO quotationDAO) {
+        this.quotationDAO = quotationDAO;
+    }
+
+    public RevisionDAO getRevisionDAO() {
+        return revisionDAO;
+    }
+
+    public void setRevisionDAO(RevisionDAO revisionDAO) {
+        this.revisionDAO = revisionDAO;
+    }
+
+    public RoleDAO getRoleDAO() {
+        return roleDAO;
+    }
+
+    public void setRoleDAO(RoleDAO roleDAO) {
+        this.roleDAO = roleDAO;
+    }
+
+    public SoftwareAgentDAO getSoftwareAgentDAO() {
+        return softwareAgentDAO;
+    }
+
+    public void setSoftwareAgentDAO(SoftwareAgentDAO softwareAgentDAO) {
+        this.softwareAgentDAO = softwareAgentDAO;
+    }
+
+    public StartDAO getStartDAO() {
+        return startDAO;
+    }
+
+    public void setStartDAO(StartDAO startDAO) {
+        this.startDAO = startDAO;
+    }
+
+    public TypeDAO getTypeDAO() {
+        return typeDAO;
+    }
+
+    public void setTypeDAO(TypeDAO typeDAO) {
+        this.typeDAO = typeDAO;
     }
 
 }
